d6a25077fa8ae8c81e273fc7e83701b63b730d88
melody-25 <m.eriksen.mem22@stud.bbbaden.ch>
2025-07-03 14:58:37 +0200

    task complete

--- tasks/Python_aufgabe.py
+++ tasks/Python_aufgabe.py
@@ -1,2 +1,5 @@
 #Schreibe "Eine Aufgabe", jedoch so, dass das Wort "Eine" auf einer Zeile geschrieben wird
-#und das Wort "Aufgabe" pro Buchstabe eine Zeile einnimmt. 
+#und das Wort "Aufgabe" pro Buchstabe eine Zeile einnimmt.
+print('Eine')
+for char in 'Aufgabe':
+    print(char)
